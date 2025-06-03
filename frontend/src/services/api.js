c24bb12c91ba06fe86937c26a63ec29ce170d972
Sagar1566 <gajbharsagar2005@gmail.com>
2025-06-04 01:07:16 +0530

    update env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://excel-analytic-platform.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
